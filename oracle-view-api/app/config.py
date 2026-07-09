43e346bcc08a9bfa9ade2f1acdde7a11df8c41fa
fabiofedele69 <41116366+fabiofedele69@users.noreply.github.com>
2026-07-09 08:37:22 +0200

    Create config.py for Oracle database settings
    Add configuration settings for Oracle database connection

---
+++ oracle-view-api/app/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    ORACLE_USER = os.getenv("ORACLE_USER")
+    ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
+    ORACLE_HOST = os.getenv("ORACLE_HOST")
+    ORACLE_PORT = os.getenv("ORACLE_PORT", "1521")
+    ORACLE_SERVICE = os.getenv("ORACLE_SERVICE")
+
+    API_HOST = "127.0.0.1"
+    API_PORT = 8000
+
+
+settings = Settings()
