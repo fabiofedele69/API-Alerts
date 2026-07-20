cb865d4a5047892931b3e79c779ce8795e1b3919
fabiofedele69 <41116366+fabiofedele69@users.noreply.github.com>
2026-07-20 15:28:40 +0200

    Update start-api-reporter.sh

--- oracle-view-api/start-api-reporter.sh
+++ oracle-view-api/start-api-reporter.sh
@@ -13,3 +13,6 @@ exec "$APP_HOME/venv/bin/python" \
     -m uvicorn app.main:app \
     --host 127.0.0.1 \
     --port 8000
+
+
+sudo -u um501001 bash -c 'export LD_LIBRARY_PATH=/app/M50/m50-openssl-1.1.1w/lib:${LD_LIBRARY_PATH:-} /app/M50/m50-api-reporter/venv/bin/python -c "import ssl; print(ssl.OPENSSL_VERSION)"'
